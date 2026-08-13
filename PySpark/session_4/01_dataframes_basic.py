f18b7ebfbbb78d77f22d0ddcf628394553fa2faa
Kushal Bendale <bendalekushal@gmail.com>
2026-08-13 14:01:27 +0530

    Add PySpark Session 4 DataFrame transformations

--- PySpark/session_4/01_dataframes_basic.py
+++ PySpark/session_4/01_dataframes_basic.py
@@ -1,3 +1,4 @@
+from pandas import col
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 
@@ -25,9 +26,51 @@ filtered_employees = employees_df\
     .filter((F.col("salary") > 60000) & (F.col("department") == "IT"))\
     .select("name", "salary", "department")
 
-filtered_employees.show()
+# result = (
+#     employees_df
+#     .filter(
+#         (F.col("salary") > 60000) |
+#         (F.col("department") == "IT")
+#     )
+#     .select("name", "department", "salary")
+# )
 
-filtered_employees.explain("formatted")
+# result.show()
+
+# result.explain("formatted")
+
+# salary_band_df = employees_df.withColumn(
+#     "salary_band",
+#     F.when(F.col("salary") < 60000, "Low")
+#     .when((F.col("salary") >= 60000) & (F.col("salary") < 80000), "Medium")
+#     .otherwise("High")
+# )
+
+# salary_band_df.show()
+
+# salary_band_df.explain("formatted")
+
+employees_messy = employees_df.withColumn(
+    "salary_string",
+    F.col("salary").cast("string")
+)
+
+employees_messy.printSchema()
+
+employees_typed = employees_messy.withColumn(
+    "salary_numeric",
+    F.col("salary_string").cast("double")
+)
+
+employees_typed.printSchema()
+employees_typed.show()
+
+employees_final = employees_typed.withColumn(
+    "country",
+    F.lit("India")
+)
+
+employees_final.show()
 
 spark.stop()
 
